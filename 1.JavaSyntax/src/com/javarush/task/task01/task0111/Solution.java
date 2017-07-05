7bafd55003876c631806ccdacafcf33f6cea68da
Vladimir Andreev <u_m0tfp@Vladimirs-MacBook-Pro.local>
2017-07-05 10:34:41 +0300

    Added. main part.

---
+++ 1.JavaSyntax/src/com/javarush/task/task01/task0111/Solution.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task01.task0111;
+
+/* 
+Одной переменной недостаточно
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 6;
+        String s = "abc";
+    }
+}
